46f0b4d841a1a9a155b553c202edcc6a3fb0fc8f
kjuulh <contact@kjuulh.io>
2023-02-12 21:02:14 +0100

    reset output

--- crates/dagger-sdk/src/gen.rs
+++ crates/dagger-sdk/src/gen.rs
@@ -5,3 +5,34 @@ pub struct FileId(String);
 pub struct Platform(String);
 pub struct SecretId(String);
 pub struct SocketId(String);
+pub struct BuildArg {
+
+}
+pub struct CacheVolume {
+}
+pub struct Container {
+}
+pub struct Directory {
+}
+pub struct EnvVariable {
+}
+pub struct File {
+}
+pub struct GitRef {
+}
+pub struct GitRepository {
+}
+pub struct Host {
+}
+pub struct HostVariable {
+}
+pub struct Label {
+}
+pub struct Project {
+}
+pub struct Query {
+}
+pub struct Secret {
+}
+pub struct Socket {
+}
